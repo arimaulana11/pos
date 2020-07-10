bf40764fe82e4f21fd895ff934f09905ecfdd288
tada1011 <dandi.webdev@gmail>
2020-07-10 14:11:03 +0700

    update first

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,3 +1,8 @@
+<li class="header">Master Data</li>
+<li class="{{ Request::is('users*') ? 'active' : '' }}">
+    <a href="{!! route('users.index') !!}"><i class="fa fa-user"></i><span>Users</span></a>
+</li>
+
 <li class="{{ Request::is('barangs*') ? 'active' : '' }}">
     <a href="{{ route('barangs.index') }}"><i class="fa fa-edit"></i><span>Barangs</span></a>
 </li>
@@ -9,7 +14,7 @@
 <li class="{{ Request::is('menus*') ? 'active' : '' }}">
     <a href="{{ route('menus.index') }}"><i class="fa fa-edit"></i><span>Menus</span></a>
 </li>
-
+<li class="header">Transaction</li>
 <li class="{{ Request::is('orders*') ? 'active' : '' }}">
     <a href="{{ route('orders.index') }}"><i class="fa fa-edit"></i><span>Orders</span></a>
 </li>
@@ -18,15 +23,23 @@
     <a href="{{ route('orderDetails.index') }}"><i class="fa fa-edit"></i><span>Order Details</span></a>
 </li>
 
-<li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{{ route('roles.index') }}"><i class="fa fa-edit"></i><span>Roles</span></a>
+<li class="header">Report</li>
+<li class="header">Setting</li>
+
+<li class="treeview">
+    <a href="#"><i class="fa fa-th"></i> <span>Role</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li class="{{ Request::is('roles*') ? 'active' : '' }}">
+            <a href="{{ route('roles.index') }}"><i class="fa fa-edit"></i><span>Roles</span></a>
+        </li>
+
+        <li class="{{ Request::is('roleDetails*') ? 'active' : '' }}">
+            <a href="{{ route('roleDetails.index') }}"><i class="fa fa-edit"></i><span>Role Details</span></a>
+        </li>
+    </ul>
 </li>
 
-<li class="{{ Request::is('roleDetails*') ? 'active' : '' }}">
-    <a href="{{ route('roleDetails.index') }}"><i class="fa fa-edit"></i><span>Role Details</span></a>
-</li>
-
-
-<li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-user"></i><span>Users</span></a>
-</li>
